3670feca46163025848447560654bea5943ca98c
Jamie Carl <jamie@hazaar.io>
2025-11-06 15:40:45 +1100

    Fix: Show default images for clubs
    Fixes the issue where clubs without banner or logo URLs
    were displayed with gray boxes or broken images.
    
    Now, default images are shown when a club doesn't have
    a banner or logo.

--- src/views/ClubDetails.vue
+++ src/views/ClubDetails.vue
@@ -180,7 +180,7 @@ onMounted(() => {
       <StackLayout row="0">
         <GridLayout class="relative">
           <Image v-if="club.BannerUrl" :src="club.BannerUrl" class="w-full h-48 object-cover" />
-          <GridLayout v-else class="w-full h-48 bg-gray-300" />
+          <Image v-else src="https://fpvtrackside.com/assets/race-D7SlXGeN.jpg" class="w-full h-48 object-cover" />
           <Image v-if="club.LogoUrl" :src="club.LogoUrl" class="h-20 w-20 object-cover rounded-lg ml-4 mt-4" 
                  verticalAlignment="top" horizontalAlignment="left" />
           <Image v-else src="https://fpvtrackside.com/assets/defaultclub-iJLq4-Em.png" 
